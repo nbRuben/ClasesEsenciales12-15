db082efd488a74a3df1bb1e68a31770ca5246613
Ruben Nuevo <nb.ruben@gmail.com>
2014-03-13 21:17:55 +0100

    ComunicacionesRed v4

---
+++ src/main/java/edu/eetac/upc/dsa/rnuevo/ComunicacionesRed/Ejercicio13/Ejercicio13_2.java
@@ -0,0 +1,24 @@
+package edu.eetac.upc.dsa.rnuevo.ComunicacionesRed.Ejercicio13;
+
+import java.net.*;
+import java.io.*;
+
+public class Ejercicio13_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			Socket cliente = new Socket("127.0.0.1", 5000);
+			DataOutputStream salida = new DataOutputStream(cliente.getOutputStream());
+			DataInputStream entrada = new DataInputStream(cliente.getInputStream());
+			
+			String hora = entrada.readUTF();
+			System.out.println(hora);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
